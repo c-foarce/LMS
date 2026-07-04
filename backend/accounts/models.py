8684c1cc041dbfd7529c9864491b934af1953981
c-foarce <dimensionripper@gmail.com>
2026-07-04 16:15:04 +0100

    minor change to Course model

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -4,7 +4,12 @@ from django.db import models
 
 class Course(models.Model):
     name = models.CharField(max_length=100)
-    code = models.CharField(max_length=20)
+    
+    code = models.CharField(
+    max_length=20,
+    null=True,
+    blank=True
+)
 
     teacher = models.ForeignKey(
         'User',
