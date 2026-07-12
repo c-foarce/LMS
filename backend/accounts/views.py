9223f2f71385a9796e4f7d5990fc2ae7cc9b34d6
c-foarce <dimensionripper@gmail.com>
2026-07-12 13:11:22 +0100

    fixed trailing slash issue, first run of server with /courses app, all seems well

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 from rest_framework import generics
 from .models import User, Enrolment
-from .serializers import RegisterSerializer, EnrolmentSerializer
+from .serializers import RegisterSerializer
 
 from rest_framework.decorators import api_view,permission_classes
 from rest_framework.permissions import IsAuthenticated
