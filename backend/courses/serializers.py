6cc118ea9287c1323edbeb8dab3d90078c316f1a
c-foarce <dimensionripper@gmail.com>
2026-07-12 12:44:58 +0100

    unsaved serializer addes to courses/serializers

---
+++ backend/courses/serializers.py
@@ -0,0 +1,21 @@
+from rest_framework import serializers
+from accounts.models import Enrolment
+
+class EnrolmentSerializer(serializers.ModelSerializer):
+    course_name = serializers.CharField(source="course.name", read_only = True)
+    course_code = serializers.CharField(source="course.code", read_only = True)
+    teacher = serializers.CharField(source="course.teacher.username", read_only=True)
+
+    class Meta:
+        model = Enrolment
+        fields= [
+            "id",
+            "course",
+            "course_name",
+            "course_code",
+            "teacher",
+            "status",
+            "progress",
+            "grade",
+            "enrolled_at",
+        ]
