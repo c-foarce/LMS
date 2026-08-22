ed055bd31a4a6b5ca3a5f99d44e9ee0d87091cf0
c-foarce <dimensionripper@gmail.com>
2026-08-22 13:46:40 +0100

    add history record component for use in History.jsx

---
+++ frontend/src/components/DisplayCards/HistoryRecord.jsx
@@ -0,0 +1,31 @@
+
+function HistoryRecord({ record }) {
+
+    const formattedDate = new Date(
+        record.completed_at
+    ).toLocaleString("en-GB", {
+        day: "2-digit",
+        month: "long",
+        year: "numeric",
+        hour: "2-digit",
+        minute: "2-digit",
+    })
+
+    return (
+        <div>
+            <h2>{record.course_name}</h2>
+
+            <p>Code: {record.course_code}</p>
+
+            <p>Student: {record.student_first_name}{" "}{record.student_last_name}</p>
+
+            <p>Teacher: {record.teacher_username}</p>
+
+            <p>Grade: {record.grade}</p>
+
+            <p>Completed: {formattedDate}</p>
+        </div>
+    )
+}
+
+export default HistoryRecord
