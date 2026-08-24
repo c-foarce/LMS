8ebc6fdeaebbc9031eed41302b12a2c912d04637
c-foarce <dimensionripper@gmail.com>
2026-08-24 22:03:31 +0100

    mock data for upcoming tests

---
+++ frontend/src/test/mocks/auth.js
@@ -0,0 +1,19 @@
+export const mockStudent = {
+    username: "teststudent",
+    role: "student",
+};
+
+export const mockTeacher = {
+    username: "testteacher",
+    role: "teacher",
+};
+
+export const mockAdmin = {
+    username: "testadmin",
+    role: "admin",
+};
+
+export const mockLoginResponse = {
+    access: "mock-access-token",
+    refresh: "mock-refresh-token",
+};
