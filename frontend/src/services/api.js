9f9e684a6be566cc057d40983e34bf84d64e2ae0
c-foarce <dimensionripper@gmail.com>
2026-09-03 17:30:52 +0100

    note for deployment

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
     baseURL: "http://127.0.0.1:8000/api"
-});
+}); //environent variable
 
 //add access token to each request made
 api.interceptors.request.use((config) => {
